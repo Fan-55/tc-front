fd036fc878b3bf3a36fe4e9d587be4ed0f4670cc
fan5 <>
2023-07-26 00:54:37 +0800

    Add store

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia';
+
+export const useLoginStore = defineStore('login', {
+  state: () => ({
+    _isLoggedIn: false,
+    _userId: null,
+    _token: "",
+    _role: "buyer",
+  }),
+  getters: {
+    // Getter to access the login state
+    isLoggedIn: state => state._isLoggedIn,
+    role: state => state._role,
+  },
+  actions: {
+    // Action to update the login state
+    setLoggedIn(value) {
+      this._isLoggedIn = value;
+    },
+    setUserId(value) {
+      this._userId = value;
+    },
+    setToken(value) {
+      this._token = value;
+    },
+    setRole(value) {
+      this._role = value;
+    }
+  },
+});
